db8977532344d075b405cf12bca8075298fd8f28
Victor Mal <VictorianusVelikyu@gmail.com>
2021-11-04 03:45:05 +0200

    Connect pages to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignUpPage from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route exact path="/signup">
+          <SignUpPage />
+        </Route>
+        
+      </Switch>
+    </BrowserRouter>
   );
 }
 
